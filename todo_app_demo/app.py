e979d898e164b2da48f6ad863df8a0a30f39af1c
yousifelhady <yousif.abdelhamed@gmail.com>
2020-12-20 23:45:38 +0200

    adding .gitignore file, implementing todo list which handle other todos, but not yet fully implemented

--- todo_app_demo/app.py
+++ todo_app_demo/app.py
@@ -9,14 +9,24 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
+
+    def __repr__(self):
+        return f'<ID: {self.id}, List Name: {self.name}>'
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
-        return f'<ID: {self.id}, TODO: {self.description}>'
+        return f'<ID: {self.id}, TODO: {self.description}, Completed: {self.completed}>'
 
 #db.create_all() #create tables if not exist
 
@@ -66,11 +76,16 @@ def delete_todo(todoId):
         db.session.close()
     return jsonify({ 'success': True })
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html',
+    lists=TodoList.query.order_by('id').all(),
+    todos=Todo.query.filter_by(list_id=list_id).order_by('id').all(),
+    active_list=TodoList.query.get(list_id))
+
 @app.route('/')
 def index():
-    return render_template('index.html',
-    data=Todo.query.order_by('id').all()
-    )
+    return redirect(url_for('get_list_todos', list_id=1))
 
 if __name__ == '__main__':
     app.run()
