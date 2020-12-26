aef2bbb29f378fad3b27b683f3302ce6bcd522f2
yousifelhady <yousif.abdelhamed@gmail.com>
2020-12-26 15:30:48 +0200

    adding my first flaskr app

---
+++ flask_setup_example/flaskr/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+import os
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+    #if not in testing mode
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    #create the instance path dir
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def hello():
+        #return a JSON response (it has to be a response)
+        return jsonify({'message': 'Hello World!'})
+
+    @app.route('/smiley')
+    def smiley():
+        return ':)'
+
+    return app
