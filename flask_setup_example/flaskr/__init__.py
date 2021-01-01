1af8a12ca56c89cc6f0a03edd7bd03514e5163da
yousifelhady <yousif.abdelhamed@gmail.com>
2021-01-01 18:50:34 +0200

    pushing changes to flaskr first setup app

--- flask_setup_example/flaskr/__init__.py
+++ flask_setup_example/flaskr/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import os
 
 def create_app(test_config=None):
@@ -17,6 +18,13 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+    
+    CORS(app)
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
 
     @app.route('/')
     def hello():
